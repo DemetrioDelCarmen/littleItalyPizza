04a9c9119b03c79d7ec7d19fa4bb5febe727c8a4
Demetrio Gomez <desarrollalab@gmail.com>
2025-09-30 16:24:50 -0600

    fixing images

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -9,70 +9,70 @@ const menuCategories = [
         name: "Pizzas",
         description: "Hand-tossed dough, homemade sauce, and fresh ingredients",
         icon: "🍕",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4pq3yhxe8w85yhnr9phsp44%2F1757404147_img_0.webp?st=2025-09-09T06%3A33%3A05Z&se=2025-09-15T07%3A33%3A05Z&sks=b&skt=2025-09-09T06%3A33%3A05Z&ske=2025-09-15T07%3A33%3A05Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=aa5ddad1-c91a-4f0a-9aca-e20682cc8969&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=TqZhT6PIvGqj6iod6tlgdsiXXSMdmbm49h3PHy8vmjo%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_0153_Gourmet%20Pizza%20Creations_simple_compose_01k4pqbxb1ez09xebfcvb25pwf.png"
     },
     {
         id: "specialty-pizzas",
         name: "Specialty Pizzas",
         description: "Our signature creations for the ultimate pizza experience",
         icon: "🌟",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4pqbwf4f7hvyqtzk7jsk21r%2F1757404409_img_1.webp?st=2025-09-09T06%3A33%3A05Z&se=2025-09-15T07%3A33%3A05Z&sks=b&skt=2025-09-09T06%3A33%3A05Z&ske=2025-09-15T07%3A33%3A05Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=aa5ddad1-c91a-4f0a-9aca-e20682cc8969&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=vobT41bfuEu0zWkpPj417pgwfZM4XDbgjSufwhHSzHE%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_0153_Gourmet%20Pizza%20Creations_simple_compose_01k4pqbxb1ez09xebfcvb25pwf.png"
     },
     {
         id: "appetizers",
         name: "Appetizers",
         description: "Start your meal with these delicious beginnings",
         icon: "🍤",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4pqs0hyfa6sqbx6664t2nmj%2F1757404815_img_0.webp?st=2025-09-09T06%3A34%3A26Z&se=2025-09-15T07%3A34%3A26Z&sks=b&skt=2025-09-09T06%3A34%3A26Z&ske=2025-09-15T07%3A34%3A26Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=aa5ddad1-c91a-4f0a-9aca-e20682cc8969&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=0OmwonhXGO3l4vp6BMMu1KumQc%2FzQEBR7zTHWHfTKXw%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_0200_Delicious%20Appetizers_simple_compose_01k4pqs18jftw99s01zyd7xwzg.png"
     },
     {
         id: "salads",
         name: "Salads",
         description: "Fresh greens with your favorite dressings",
         icon: "🥗",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4pqwzgpedrb7tw9bfb3dn0t%2F1757404945_img_0.webp?st=2025-09-09T07%3A01%3A48Z&se=2025-09-15T08%3A01%3A48Z&sks=b&skt=2025-09-09T07%3A01%3A48Z&ske=2025-09-15T08%3A01%3A48Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=aa5ddad1-c91a-4f0a-9aca-e20682cc8969&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=weJRgb6%2B0m1YkAn%2BLbafnojxFppqdvKF2ykCqs%2BG64w%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_0202_Fresh%20Green%20Salad_simple_compose_01k4pqx25rf01rcjd3h4h76g97.png"
     },
     {
         id: "wings",
         name: "Wings",
         description: "Crispy wings with your choice of sauce",
         icon: "🍗",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4pr19j3fzm9wtqede7pqxky%2F1757405086_img_0.webp?st=2025-09-09T06%3A34%3A39Z&se=2025-09-15T07%3A34%3A39Z&sks=b&skt=2025-09-09T06%3A34%3A39Z&ske=2025-09-15T07%3A34%3A39Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=aa5ddad1-c91a-4f0a-9aca-e20682cc8969&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=ujK%2B7%2Fh%2FIFeEoJdWMdvO0ZGRoU9Ha2yVnSFptQqNn5c%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_0204_Saucy%20Crispy%20Wings_simple_compose_01k4pr1bsff5at16d3qfe6ev4x.png"
     },
     {
         id: "calzones-strombolis",
         name: "Calzones & Strombolis",
         description: "Baked, folded dough stuffed with delicious fillings",
         icon: "🥟",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4pre8ncft7re1d2884m45v7%2F1757405512_img_0.webp?st=2025-09-09T06%3A34%3A40Z&se=2025-09-15T07%3A34%3A40Z&sks=b&skt=2025-09-09T06%3A34%3A40Z&ske=2025-09-15T07%3A34%3A40Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=aa5ddad1-c91a-4f0a-9aca-e20682cc8969&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=fA6jSuAcxAfsf0TEnI7FjeGtmGhnH%2F8K8tHewf10RoE%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_0211_Calzones%20and%20Strombolis_simple_compose_01k4preat7ey5sahs43w09wrqx.png"
     },
     {
         id: "sandwiches-subs",
         name: "Sandwiches & Subs",
         description: "Freshly made sandwiches on brioche rolls",
         icon: "🥪",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4prm79jfy3a56gryy193xva%2F1757405679_img_0.webp?st=2025-09-09T06%3A34%3A34Z&se=2025-09-15T07%3A34%3A34Z&sks=b&skt=2025-09-09T06%3A34%3A34Z&ske=2025-09-15T07%3A34%3A34Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=aa5ddad1-c91a-4f0a-9aca-e20682cc8969&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=KamSQi2KVMMumTbJdtoPBbnWL1zuPvxH2baY7fzBUes%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_0214_Delicious%20Sandwiches%20Display_simple_compose_01k4prm9g2e89avxy55atgdw4v.png"
     },
     {
         id: "pasta-dinners",
         name: "Pasta Dinners",
         description: "Classic Italian pasta dishes served with salad and bread",
         icon: "🍝",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4qvwq7cfzxssgdkc2f1pg58%2F1757442692_img_0.webp?st=2025-09-09T17%3A19%3A55Z&se=2025-09-15T18%3A19%3A55Z&sks=b&skt=2025-09-09T17%3A19%3A55Z&ske=2025-09-15T18%3A19%3A55Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=cfbc986b-d2bc-4088-8b71-4f962129715b&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=rglxsAuzpJu9rC%2FkvpiW5TClKfAoAHEU6iLqD8fuwNc%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_1231_Italian%20Pasta%20Feast_simple_compose_01k4qvwt5vee0vv7hc35vs6bf4.png"
     },
     {
         id: "dinners",
         name: "Dinners",
         description: "Complete meals including baked and specialty dinners",
         icon: "🍽️",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4qwpkmge6haptrqadxrnxnq%2F1757443508_img_0.webp?st=2025-09-09T17%3A12%3A25Z&se=2025-09-15T18%3A12%3A25Z&sks=b&skt=2025-09-09T17%3A12%3A25Z&ske=2025-09-15T18%3A12%3A25Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=cfbc986b-d2bc-4088-8b71-4f962129715b&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=sTyQmpRhHBYazkGlqXOS%2Fox2ixU9sEJ937YVkF%2BUXLc%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_1245_Delicious%20Dinner%20Spread_simple_compose_01k4qwpp4af93t2djk9604p1zs.png"
     },
     {
         id: "desserts",
         name: "Desserts",
         description: "Sweet endings to your Italian feast",
         icon: "🍰",
-        image: "https://videos.openai.com/vg-assets/assets%2Ftask_01k4qwv87nfr7a3sbhrz8r1dpy%2F1757443688_img_0.webp?st=2025-09-09T17%3A14%3A55Z&se=2025-09-15T18%3A14%3A55Z&sks=b&skt=2025-09-09T17%3A14%3A55Z&ske=2025-09-15T18%3A14%3A55Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=cfbc986b-d2bc-4088-8b71-4f962129715b&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=bZi5W0cWlsW1en6U5urAMGw2V%2BdC8LsegtP5Qt%2F9Ej0%3D&az=oaivgprodscus"
+        image: "https://rayrtuqztdc8phcw.public.blob.vercel-storage.com/littleItaly/menu/20250909_1248_Italian%20Dessert%20Delights_simple_compose_01k4qwv9cferza17bx3mmrp04c.png"
     },
     {
         id: "beverages",
